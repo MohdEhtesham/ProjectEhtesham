20c950afa45eedfa7a38db9fb49565c7db3c8ab5
Mohd. Ehtesham <101743107+MohdEhtesham@users.noreply.github.com>
2023-04-13 16:52:16 +0530

    chat

--- App.tsx
+++ App.tsx
@@ -1,88 +1,88 @@
-import React, {useState, useEffect} from 'react';
-import {StripeProvider} from '@stripe/stripe-react-native';
+// import React, {useState, useEffect} from 'react';
+// import {StripeProvider} from '@stripe/stripe-react-native';
 
-import {StyleSheet, Button, View, Text} from 'react-native';
-import {useStripe} from '@stripe/stripe-react-native';
+// import {StyleSheet, Button, View, Text} from 'react-native';
+// import {useStripe} from '@stripe/stripe-react-native';
 
-const App = () => {
-  const API_URL = 'http://localhost:8000';
-  const {initPaymentSheet, presentPaymentSheet} = useStripe();
-  const [loading, setLoading] = useState(false);
+// const App = () => {
+//   const API_URL = 'http://localhost:8000';
+//   const {initPaymentSheet, presentPaymentSheet} = useStripe();
+//   const [loading, setLoading] = useState(false);
 
-  const fetchPaymentSheetParams = async () => {
-    const response = await fetch(`${API_URL}/create-payment-intent`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-        console.log("=========123444444",response)
-    const {clientSecret, customer} = await response.json();
+//   const fetchPaymentSheetParams = async () => {
+//     const response = await fetch(`${API_URL}/create-payment-intent`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//         console.log("=========123444444",response)
+//     const {clientSecret, customer} = await response.json();
 
-    return {
-      clientSecret,
-      customer,
-    };
-  };
-  const initializePaymentSheet = async () => {
-    const {clientSecret, customer} = await fetchPaymentSheetParams();
+//     return {
+//       clientSecret,
+//       customer,
+//     };
+//   };
+//   const initializePaymentSheet = async () => {
+//     const {clientSecret, customer} = await fetchPaymentSheetParams();
 
-    const {error} = await initPaymentSheet({
-      customerId: customer,
-      paymentIntentClientSecret: clientSecret,
-      merchantDisplayName: 'Merchant Name',
-    });
-    if (!error) {
-      setLoading(true);
-    }
-  };
-  const openPaymentSheet = async () => {
-    const {error} = await presentPaymentSheet();
-    if (error) {
-      console.log(`Error code: ${error.code}`, error.message);
-    } else {
-      console.log('Success', 'Your order is confirmed!');
-    }
-  };
-  useEffect(() => {
-    initializePaymentSheet();
-  }, []);
+//     const {error} = await initPaymentSheet({
+//       customerId: customer,
+//       paymentIntentClientSecret: clientSecret,
+//       merchantDisplayName: 'Merchant Name',
+//     });
+//     if (!error) {
+//       setLoading(true);
+//     }
+//   };
+//   const openPaymentSheet = async () => {
+//     const {error} = await presentPaymentSheet();
+//     if (error) {
+//       console.log(`Error code: ${error.code}`, error.message);
+//     } else {
+//       console.log('Success', 'Your order is confirmed!');
+//     }
+//   };
+//   useEffect(() => {
+//     initializePaymentSheet();
+//   }, []);
 
-  return (
-    <StripeProvider publishableKey="pk_test_51MvGhOSECuCbmaL04sAagvSh6bknn5RtjiRb2YVlRyI69yoiZzoY1xbxHQNmobOC9bp8Mkd555dWu5i15QdFLAzi00aR5tshaU">
-      <View style={styles.container}>
-        {!loading ? (
-          <Text style={{color: 'black'}}>Loading...</Text>
-        ) : (
-          <Button
-            style={styles.button}
-            disabled={!loading}
-            title="Checkout"
-            color="#841584"
-            onPress={openPaymentSheet}
-          />
-        )}
-      </View>
-    </StripeProvider>
-  );
-};
+//   return (
+//     <StripeProvider publishableKey="pk_test_51MvGhOSECuCbmaL04sAagvSh6bknn5RtjiRb2YVlRyI69yoiZzoY1xbxHQNmobOC9bp8Mkd555dWu5i15QdFLAzi00aR5tshaU">
+//       <View style={styles.container}>
+//         {!loading ? (
+//           <Text style={{color: 'black'}}>Loading...</Text>
+//         ) : (
+//           <Button
+//             style={styles.button}
+//             disabled={!loading}
+//             title="Checkout"
+//             color="#841584"
+//             onPress={openPaymentSheet}
+//           />
+//         )}
+//       </View>
+//     </StripeProvider>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    marginHorizontal: 100,
-    marginVertical: 100,
-  },
-  button: {
-    backgroundColor: '#00aeef',
-    borderColor: 'red',
-    borderWidth: 5,
-    borderRadius: 15,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     marginHorizontal: 100,
+//     marginVertical: 100,
+//   },
+//   button: {
+//     backgroundColor: '#00aeef',
+//     borderColor: 'red',
+//     borderWidth: 5,
+//     borderRadius: 15,
+//   },
+// });
 
-export default App;
+// export default App;
 
 
 
@@ -110,39 +110,39 @@ export default App;
 
 
 
-// import React from 'react';
-// import { createStackNavigator } from '@react-navigation/stack'
-// import { NavigationContainer } from '@react-navigation/native';
-// import Login from './src/screens/Login';
-// import Register from './src/screens/Register';
-// import Chat from './src/screens/Chat';
-// import Splash from './src/screens/Splash';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+import Chat from './src/screens/Chat';
+import Splash from './src/screens/Splash';
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-// const App = () => {
-//   return (
-//     <NavigationContainer  >
-//       <Stack.Navigator screenOptions={{
-//         headerShown:false
-//       }}>
-//         <Stack.Screen name='Splash' component={Splash} />
-//         <Stack.Screen name='Login' component={Login} />
-//         <Stack.Screen name='Register' component={Register} />
-//         <Stack.Screen name='Chat' component={Chat}
-// //  options={{
-// //   title: 'Chat',
-// //   headerStyle: {
-// //     backgroundColor: 'blue',
-// //   }
-// // }}
-//        />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
+const App = () => {
+  return (
+    <NavigationContainer  >
+      <Stack.Navigator screenOptions={{
+        headerShown:false
+      }}>
+        <Stack.Screen name='Splash' component={Splash} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Chat' component={Chat}
+//  options={{
+//   title: 'Chat',
+//   headerStyle: {
+//     backgroundColor: 'blue',
+//   }
+// }}
+       />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-// export default App;
+export default App;
 
 // import React, { useEffect } from 'react';
 // import {
